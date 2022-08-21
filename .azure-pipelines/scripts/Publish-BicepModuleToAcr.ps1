9b782b15eb957294675da840af4998b2e434bf62
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-08-21 15:23:36 +0100

    Partially aligning with MS learn docs

--- .azure-pipelines/scripts/Publish-BicepModuleToAcr.ps1
+++ .azure-pipelines/scripts/Publish-BicepModuleToAcr.ps1
@@ -1,14 +1,13 @@
 param (
     [string] $moduleName,
     [string] $modulesRootPath,
-    [string] $metadataRootPath,
     [string] $acrName,
     [string] $repositoryPrefix = "bicep/modules",
     [boolean] $previewRelease = $true
 )
 
-$moduleFilePath = Join-Path $modulesRootPath "$moduleName.bicep"
-$metadataFilePath = Join-Path $metadataRootPath "$moduleName.json"
+$moduleFilePath = Join-Path $modulesRootPath $moduleName "main.bicep"
+$metadataFilePath = Join-Path $modulesRootPath $moduleName "metadata.json"
 $acrRepository = "$repositoryPrefix/$moduleName".ToLower()
 
 if ((Test-Path $moduleFilePath) -eq $false) {
@@ -17,43 +16,38 @@ if ((Test-Path $moduleFilePath) -eq $false) {
 }
 
 if ((Test-Path $metadataFilePath) -eq $false) {
-    Write-Error "The module at '$moduleFilePath' could not be found!"
+    Write-Error "The metadata file at '$metadataFilePath' could not be found!"
     exit
 }
 
 Write-Host "Using metadata file: '$metadataFilePath'"
 $moduleMetadata = Get-Content $metadataFilePath | ConvertFrom-Json
 
-$moduleTag = "V$($moduleMetadata.version)"
-if ($previewRelease) {
-    $moduleTag = "$moduleTag-preview"
-}
+$tagsToPushTo = @()
 
-Write-Host "Setting the module version tag to: '$moduleTag'"
-
-$publishModule = $false
+$majorMinorRevisionVersion = "V$($moduleMetadata.version.major).$($moduleMetadata.version.minor).$($moduleMetadata.version.revision)"
+$majorMinorXRevisionVersion = "V$($moduleMetadata.version.major).$($moduleMetadata.version.minor).x"
 
 $repositories = az acr repository list --name $acrName | ConvertFrom-Json
 if ($null -eq $repositories -or !$repositories.Contains($acrRepository)) {
-    $publishModule = $true
+    $tagsToPushTo.Add($majorMinorRevisionVersion)
+    $tagsToPushTo.Add($majorMinorXRevisionVersion)
+    $tagsToPushTo.Add("latest")
 }
 else {
     $moduleTags = az acr repository show-tags --name $acrName --repository $acrRepository | ConvertFrom-Json
 
-    if ($moduleTags.Contains($moduleTag)) {
-        Write-Warning "There is already a published image with the tag '$moduleTag'"
+    if ($moduleTags.Contains($majorMinorRevisionVersion)) {
+        Write-Warning "There is already a published image with the tag '$majorMinorRevisionVersion'"
     }
     else {
-        $publishModule = $true
+        $tagsToPushTo.Add($majorMinorRevisionVersion)
+        $tagsToPushTo.Add($majorMinorXRevisionVersion)
+        $tagsToPushTo.Add("latest")
     }
 }
 
-if ($publishModule) {
-    Write-Host "Publishing new module to: 'br:$acrName.azurecr.io/${acrRepository}' with tag: '$moduleTag'"
-    az bicep publish --file $moduleFilePath --target "br:$acrName.azurecr.io/${acrRepository}:$moduleTag"
-}
-
-if ($publishModule -and $previewRelease -eq $false) {
-    Write-Host "Publishing new module to: 'br:$acrName.azurecr.io/${acrRepository}' with tag: 'latest'"
-    az bicep publish --file $moduleFilePath --target "br:$acrName.azurecr.io/${acrRepository}:latest"
+$tagsToPushTo | ForEach-Object {
+    Write-Host "Publishing module to: 'br:$acrName.azurecr.io/${acrRepository}' with tag: '$_'"
+    az bicep publish --file $moduleFilePath --target "br:$acrName.azurecr.io/${acrRepository}:$_"
 }
