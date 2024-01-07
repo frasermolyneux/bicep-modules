31be3f34fbae76b4fd88fc4e0042599683dcb0d6
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-01-07 15:06:59 +0000

    No idea

--- scripts/CreateAppRegistration.sh
+++ scripts/CreateAppRegistration.sh
@@ -18,5 +18,5 @@ fi
 
 if [ ! -z "$appRoles" ]
 then
-    az ad app update --id "$applicationId" --app-roles '"'"$appRoles"'"' > /dev/null
+    az ad app update --id "$applicationId" --app-roles "$($appRoles | jq -r $appRoles)" > /dev/null
 fi
