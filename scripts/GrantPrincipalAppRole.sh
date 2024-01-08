e6166749123686dffb48e5cd1c09d1212f2838c5
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-01-08 18:04:41 +0000

    Adding new script

---
+++ scripts/GrantPrincipalAppRole.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+principalId=$1
+serverApplicationName=$2
+roleName=${3:-ServiceAccount}
+
+serverApplicationId=$(az ad app list --filter "displayName eq '$serverApplicationName'" --query '[].appId' | jq -r '.[]')
+serverResourceId=$(az ad sp list --filter "appId eq '$serverApplicationId'" --query '[0].id' | jq -r '.')
+serverApiSpn=$(az rest -m GET -u https://graph.microsoft.com/v1.0/servicePrincipals/$serverResourceId | jq -r '.')
+serverAppRoleId=$(echo $serverApiSpn | jq -r --arg roleName "$roleName" '.appRoles[] | select(.displayName == $roleName) | .id')
+
+permissions=$(az rest -m GET -u https://graph.microsoft.com/v1.0/servicePrincipals/$clientResourceId/appRoleAssignments | jq -r '.')
+if [[ -z $(echo $permissions | jq -r --arg serverAppRoleId "$serverAppRoleId" '.value[] | select(.appRoleId == $serverAppRoleId)') ]]; then
+    az rest -m POST -u https://graph.microsoft.com/v1.0/servicePrincipals/$clientResourceId/appRoleAssignments -b "{'principalId': '$principalId', 'resourceId': '$serverResourceId','appRoleId': '$serverAppRoleId'}"
+fi
